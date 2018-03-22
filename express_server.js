444eddc6c9da3ad8666302bf8af372fb291c95e2
Anhad Gill <gillanhad23@gmail.com>
2018-03-22 00:01:26 +0000

    Adds logout button

--- express_server.js
+++ express_server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+const bodyParser = require("body-parser");
+
 var PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs")
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+var cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,13 +23,20 @@ function generateRandomString() {
 
 // Home page
 app.get('/urls', (req, res) => {
-    let templateVars = {urls: urlDatabase};
+   // console.log('username: ', req.cookies.username);
+    let templateVars = {
+        urls: urlDatabase,
+        username: req.cookies['username']
+    };
     res.render('urls_index', templateVars);
 });
 
 // Renders new page to add a new URL
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    let templateVars = {
+        username: req.cookies["username"]
+    }
+    res.render("urls_new", templateVars);
 });
 
 // Creates a random shortURL and assigns it to the new long URL
@@ -37,10 +48,13 @@ app.post("/urls", (req, res) => {
 
 // Displays the page after a new URL is added
 app.get("/urls/:id", (req, res) => {
-    let templateVars = { shortURL: req.params.id };
+    let templateVars = {
+        shortURL: req.params.id,
+        username: req.cookies["username"]
+    };
     console.log('templateVars: ', templateVars)
     res.render("urls_show", templateVars);
-  });``
+  });
 
 // Redirects from shortURL to longURL
 app.get("/u/:shortURL", (req, res) => {
@@ -52,17 +66,27 @@ app.get("/u/:shortURL", (req, res) => {
 app.post('/urls/:id/delete', (req, res) => {
     delete urlDatabase[req.params.id];
     res.redirect('/urls');
-
 });
 
 // Updates the longURL
 app.post('/urls/:id', (req, res) => {
-    console.log('req.params: ', req.params.id);
-    console.log('req.body.updatedLongURL: ', req.body.updatedLongURL);
     urlDatabase[req.params.id] = req.body.updatedLongURL;
     res.redirect('/urls');
 });
 
+// Login
+app.post('/login', (req, res) => {
+    //might need to modify the way cookie is set
+    res.cookie('username', req.body.username);
+    //req.session.user_id = userRandomID;
+    res.redirect('/urls');
+});
+
+// Logout
+app.post('/logout', (req, res) => {
+    res.clearCookie("username");
+    res.redirect('/urls');
+});
 
 // It runs the program
 app.listen(PORT, () => {
