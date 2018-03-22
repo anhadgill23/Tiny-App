80f8d57fdf73762730e6a8b78a7f60f893a19df3
Anhad Gill <gillanhad23@gmail.com>
2018-03-22 18:19:11 +0000

    Adds a seperate login page. User can no longer login from the header.

--- express_server.js
+++ express_server.js
@@ -11,6 +11,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 var cookieParser = require('cookie-parser')
 app.use(cookieParser());
 
+// Users' data object
+const users = {
+    "userRandomID": {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur"
+    },
+   "user2RandomID": {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk"
+    }
+}
+
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -26,33 +40,32 @@ app.get('/urls', (req, res) => {
    // console.log('username: ', req.cookies.username);
     let templateVars = {
         urls: urlDatabase,
-        username: req.cookies['username']
+        user_id: users[user_id]
     };
     res.render('urls_index', templateVars);
 });
 
-// Renders new page to add a new URL
+// Renders a new page to add a new URL
 app.get("/urls/new", (req, res) => {
     let templateVars = {
-        username: req.cookies["username"]
+        user_id: users[user_id]
     }
     res.render("urls_new", templateVars);
 });
 
-// Creates a random shortURL and assigns it to the new long URL
+// Creates a random shortURL and assigns it to the long URL
 app.post("/urls", (req, res) => {
     let random_short_url = generateRandomString()
     urlDatabase[random_short_url] = 'http://' + req.body.longURL
     res.redirect(`/urls/${random_short_url}`);
 });
 
-// Displays the page after a new URL is added
+// Update URL page
 app.get("/urls/:id", (req, res) => {
     let templateVars = {
         shortURL: req.params.id,
-        username: req.cookies["username"]
+        user_id: users[user_id]
     };
-    console.log('templateVars: ', templateVars)
     res.render("urls_show", templateVars);
   });
 
@@ -62,7 +75,7 @@ app.get("/u/:shortURL", (req, res) => {
     res.redirect(longURL);
 });
 
-// Deletes short and long URL
+// Deletes URL
 app.post('/urls/:id/delete', (req, res) => {
     delete urlDatabase[req.params.id];
     res.redirect('/urls');
@@ -74,21 +87,67 @@ app.post('/urls/:id', (req, res) => {
     res.redirect('/urls');
 });
 
-// Login
+// Login check
 app.post('/login', (req, res) => {
-    //might need to modify the way cookie is set
-    res.cookie('username', req.body.username);
-    //req.session.user_id = userRandomID;
-    res.redirect('/urls');
+if (req.body.email && req.body.password) {
+    for (userObject in users) {
+        if (userObject.email === req.body.email && userObject.password === req.body.password) {
+            res.cookie('user_id', userObject.id);
+            res.redirect('/urls');
+        }
+        else if (userObject.email === req.body.email && userObject.password !== req.body.password){
+            res.status(403).send('Email or password do not match. Please try again.');
+        }
+        else {
+            res.status(403).send('This email cannot be found. Please try again.');
+        }
+    };
+}
+else {
+    res.status(400).send('Email or password cannot be empty. Please try again.');
+}
 });
 
 // Logout
 app.post('/logout', (req, res) => {
-    res.clearCookie("username");
+    res.clearCookie("user_id");
     res.redirect('/urls');
 });
 
-// It runs the program
+// Renders a new registration page
+app.get('/register', (req, res) => {
+res.render('register');
+});
+
+// Performs error checks and registers the new user
+app.post("/register", (req, res) => {
+    if (req.body.email && req.body.password) {
+        for (userObject in users) {
+            if (userObject.email === req.body.email) {
+                res.status(400).send('This email is already in use. Please try again.');
+            }
+        };
+
+        let user_id = generateRandomString();
+        users[user_id] = {
+        id: user_id,
+        email: req.body.email,
+        password: req.body.password
+        }
+        res.cookie('user_id', users[user_id].id);
+        res.redirect('/urls');
+    }
+    else {
+        res.status(400).send('Email or password cannot be empty. Please try again.');
+    }
+});
+
+// Renders a new login page
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Where it all comes together
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
-})
+});
